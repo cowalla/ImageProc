d7357390229cd58c0d20f1a9afda442612a83792
Connor Wallace <wallaconno@gmail.com>
2014-02-08 14:40:17 -0800

    create setup for simple image altering scheme (binary RGB image)

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 img = Image.open("use.jpg")
 x, y = img.size[0], img.size[1]
+total_length = x*y
 pixelMap = img.load()
 data = img.getdata()
 #data is an array of sets, each one representing a pixel value. the array is flattened
@@ -11,3 +12,8 @@ def on_off(value, eps):
 		return 0
 	else
 		return 100
+		
+#plan now is to:
+# create a function that will set all pixels in data to on or off
+# render that as an image file
+# write it to an external file
