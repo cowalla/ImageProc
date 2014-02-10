0739a7886a60f226d103b4c2ef75dab192eda37d
Connor Wallace <wallaconno@gmail.com>
2014-02-10 10:08:29 -0800

    initial work on creating simple RGB filter

--- test.py
+++ test.py
@@ -8,12 +8,27 @@ data = img.getdata()
 #data is an array of sets, each one representing a pixel value. the array is flattened
 
 def on_off(value, eps):
-	if value < eps
+	if value < eps:
 		return 0
-	else
+	else:
 		return 100
-		
+
+	
 #plan now is to:
 # create a function that will set all pixels in data to on or off
 # render that as an image file
 # write it to an external file
+
+# 
+# in the i-dir, the +1 elt is i + 1. in the j-dir the +1 elt is j + x
+ep = 30
+for i in range(0, x * y):
+	if i % x != 0 and i % x != ( x - 1 ):
+		# i is not on a horizontal boundary (important when performing NN calcs)
+		if i > x and i < x * ( y - 1 ):
+			print i
+			# i is not on a vertical boundary
+			temp = data[i]
+			data[i] = (on_off(temp[i][0], ep), on_off(temp[i][1], ep), on_off(temp[i][2], ep))
+
+data
