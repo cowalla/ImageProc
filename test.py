a3af44d558acd9d6d044bc5506fd9d5e3192d34e
Connor Wallace <wallaconno@gmail.com>
2014-02-10 10:58:01 -0800

    program works but flips images and doesnt like my boundary rules

--- test.py
+++ test.py
@@ -8,27 +8,12 @@ data = img.getdata()
 from copy import deepcopy
 #data is an array of sets, each one representing a pixel value. the array is flattened
 
-def on_off(value, eps):
-	if value < eps:
-		return 0
-	else:
-		return 100
-
-	
-#plan now is to:
-# create a function that will set all pixels in data to on or off
-# render that as an image file
-# write it to an external file
-
-# 
-# in the i-dir, the +1 elt is i + 1. in the j-dir the +1 elt is j + x
-ep = 30
 outData = []
-for i in range(0, x * y):
-	if i % y != 0 and i % y != ( y - 1 ):
-		# i is not on a horizontal boundary (important when performing NN calcs)
-		if i > y and i < y * ( x - 1 ):
-			# i is not on a vertical boundary
+for i in range(0, total_length):
+	# if i % y != 0 and i % y != ( y - 1 ):
+	# 	# i is not on a horizontal boundary (important when performing NN calcs)
+		if i > x and i < (x - 1) * ( y ):
+	# 		# i is not on a vertical boundary
 			outData.append(data[i])
 			
 img.putdata(outData)
