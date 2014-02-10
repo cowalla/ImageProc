9ca2aaf2f2376326c4a50b6831f666b1e654cf5e
Connor Wallace <wallaconno@gmail.com>
2014-02-10 10:47:26 -0800

    start exporting images that dont quite make sense

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ x, y = img.size[0], img.size[1]
 total_length = x*y
 pixelMap = img.load()
 data = img.getdata()
+from copy import deepcopy
 #data is an array of sets, each one representing a pixel value. the array is flattened
 
 def on_off(value, eps):
@@ -22,13 +23,13 @@ def on_off(value, eps):
 # 
 # in the i-dir, the +1 elt is i + 1. in the j-dir the +1 elt is j + x
 ep = 30
+outData = []
 for i in range(0, x * y):
-	if i % x != 0 and i % x != ( x - 1 ):
+	if i % y != 0 and i % y != ( y - 1 ):
 		# i is not on a horizontal boundary (important when performing NN calcs)
-		if i > x and i < x * ( y - 1 ):
-			print i
+		if i > y and i < y * ( x - 1 ):
 			# i is not on a vertical boundary
-			temp = data[i]
-			data[i] = (on_off(temp[i][0], ep), on_off(temp[i][1], ep), on_off(temp[i][2], ep))
-
-data
+			outData.append(data[i])
+			
+img.putdata(outData)
+img.save("changed.jpg", "JPEG")
